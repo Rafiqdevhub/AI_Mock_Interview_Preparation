573a4d6d41b507a891fb135357ecede36296e94f
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-03-25 21:16:54 +0500

    Add logout button to layout and improve navigation structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
-import { isAuthenticated } from "@/lib/actions/auth.action";
+import { Button } from "@/components/ui/button";
+import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
@@ -11,11 +12,15 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="root-layout">
-      <nav>
+      <nav className="flex justify-between items-center">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">MockAI</h2>
         </Link>
+
+        <form action={signOut}>
+          <Button className="btn-secondary">Logout</Button>
+        </form>
       </nav>
 
       {children}
